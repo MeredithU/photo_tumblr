b894c274d0e4839b33a8b9ede23ba6d5b22b2a37
Meredith Underell <m.underell@gmail.com>
2013-08-24 20:41:53 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoTumblr::Application.config.secret_token = '448b79ec48eac76d7785d2b87468f72c15a2a290ce31e6afd7db67ba06ea99c56e8945e960d9dcbdcd5344bbdb70313decc2c2adffd2b817df3c767e9d3b4057'
